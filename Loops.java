487416fcb6062ca441dead8d5546dd4e81029399
apcs <apcs@mvhs-u52810.dcsdk12.org>
2013-09-03 08:03:23 -0600

    Working While Loop

--- Loops.java
+++ Loops.java
@@ -1,5 +1,10 @@
 public class Loops {
 	public static void main(String[] args) {
-		System.out.println("Loops");
+	
+		int x = 1; // start
+		while (x <= 100) { // stopping condition
+			System.out.println(x); // loop action
+			x++;
+		}
 	}
 }
